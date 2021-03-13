2fd1f7614412aefd6ed92b9a3835460fa510688a
CaptainAlekor <79224183+CaptainAlekor@users.noreply.github.com>
2021-03-13 09:16:57 +0300

    Add files via upload

---
+++ LR2_3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LR2_3
+{
+    class Program
+    {
+        static void CountDigits(int digit, string str)
+        {
+            int counter = 0;
+            int pointer = 0;
+            while (pointer < str.Length)
+            {
+                if (str[pointer] == Convert.ToChar(48 + digit)) counter++;
+                pointer++;
+            }
+            if (counter == 1) Console.WriteLine($"The number {digit} was repeated 1 time");
+            else Console.WriteLine($"The number {digit} was repeated {counter} times");
+        }
+        static void Main(string[] args)
+        {
+            DateTime ThisMoment = new DateTime();
+            ThisMoment = DateTime.Now;
+            Console.WriteLine(ThisMoment);
+            Console.WriteLine($"{ThisMoment.ToLongDateString()}   {ThisMoment.ToLongTimeString()}");
+            string str = Convert.ToString(ThisMoment);
+            for (int digit = 0; digit < 10; digit++)
+            {
+                CountDigits(digit, str);
+            }
+        }
+    }
+}
