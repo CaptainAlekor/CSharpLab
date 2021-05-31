987b311f14164605bdb605c9cffa45138e4c5e35
CaptainAlekor <79224183+CaptainAlekor@users.noreply.github.com>
2021-05-31 22:07:25 +0300

    Add files via upload

---
+++ LR7/Fraction.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace LR7
+{
+    class Fraction : IComparable
+    {
+        int numerator = 0;
+        int denominator = 1;
+
+        public Fraction() { }
+        public Fraction(int numerator, int denominator)
+        {
+            if (denominator == 0) throw new Exception("Denominator cannot be equal to zero");
+            this.numerator = numerator;
+            this.denominator = denominator;
+            this.Simplify();
+        }
+
+        public int Numerator { get { return numerator; } }
+        public int Denominator { get { return denominator; } }
+
+        public static explicit operator int(Fraction fraction)
+        {
+            double result = (double)fraction.numerator / (double)fraction.denominator;
+            return (int)Math.Round(result, MidpointRounding.AwayFromZero);
+        }
+        public static explicit operator double(Fraction fraction)
+        {
+            return (double)fraction.numerator / (double)fraction.denominator;
+        }
+        public static Fraction operator +(Fraction first, Fraction second)
+        {
+            int resultNumerator = first.numerator * second.denominator + second.numerator * first.denominator;
+            int resultDenominator = first.denominator * second.denominator;
+            return new Fraction(resultNumerator, resultDenominator);
+        }
+        public static Fraction operator -(Fraction first, Fraction second)
+        {
+            int resultNumerator = first.numerator * second.denominator - second.numerator * first.denominator;
+            int resultDenominator = first.denominator * second.denominator;
+            return new Fraction(resultNumerator, resultDenominator);
+        }
+        public static Fraction operator *(Fraction first, Fraction second)
+        {
+            return new Fraction(first.numerator * second.numerator, first.denominator * second.denominator);
+        }
+        public static Fraction operator /(Fraction first, Fraction second)
+        {
+            if (second.numerator == 0) throw new Exception("Division by zero is prohibited");
+            else
+            {
+                return new Fraction(first.numerator * second.denominator, first.denominator * second.numerator);
+            }
+        }
+        public static bool operator >(Fraction first, Fraction second)
+        {
+            Fraction subtraction = first - second;
+            if (subtraction.numerator > 0) return true;
+            else return false;
+        }
+        public static bool operator <(Fraction first, Fraction second)
+        {
+            Fraction subtraction = second - first;
+            if (subtraction.numerator > 0) return true;
+            else return false;
+        }
+        public static bool operator >=(Fraction first, Fraction second)
+        {
+            Fraction subtraction = first - second;
+            if (subtraction.numerator >= 0) return true;
+            else return false;
+        }
+        public static bool operator <=(Fraction first, Fraction second)
+        {
+            Fraction subtraction = second - first;
+            if (subtraction.numerator >= 0) return true;
+            else return false;
+        }
+        public static bool operator ==(Fraction first, Fraction second)
+        {
+            if (first.numerator == second.numerator && first.denominator == second.denominator) return true;
+            else return false;
+        }
+        public static bool operator !=(Fraction first, Fraction second)
+        {
+            if (first.numerator == second.numerator && first.denominator == second.denominator) return false;
+            else return true;
+        }
+
+        private void Simplify()
+        {
+            int p = this.numerator;
+            int q = this.denominator;
+            int r;
+            while (true)
+            {
+                r = p - (p / q) * q;
+                if (r == 0) break;
+                else
+                {
+                    p = q;
+                    q = r;
+                }
+            }
+            if (q != 1)
+            {
+                this.numerator /= q;
+                this.denominator /= q;
+            }
+            if (this.denominator < 0)
+            {
+                this.denominator *= -1;
+                this.numerator *= -1;
+            }
+        }
+        public int CompareTo(object o)
+        {
+            Fraction fo = o as Fraction;
+            if (this > fo) return 1;
+            else if (this < fo) return -1;
+            else return 0;
+        }
+        public override bool Equals(object o)
+        {
+            Fraction fo = o as Fraction;
+            if (this == fo) return true;
+            else return false;
+        }
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+            result.Append(this.numerator);
+            result.Append('/');
+            result.Append(this.denominator);
+            return result.ToString();
+        }
+        public string ToSpecialString()
+        {
+            StringBuilder result = new StringBuilder();
+            result.Append(this.numerator);
+            result.Append('*');
+            result.Append(this.denominator);
+            result.Append("^(-1)");
+            return result.ToString();
+        }
+        public void ConvertToFraction(string input)
+        {
+            StringBuilder strbuilder = new StringBuilder(input);
+            bool error = false;
+            int num = 0, denom = 1;
+            do
+            {
+                if (error)
+                {
+                    Console.WriteLine("Invalid input. Possible formats:\n<number>/<number>\n-<number>/<number>\n<number>\n\nTry again: ");
+                    strbuilder.Clear();
+                    strbuilder.Append(Console.ReadLine());
+                }
+                error = false;
+                string[] constituents = strbuilder.ToString().Split('/');
+                if (constituents.Length == 1)
+                {
+                    denom = 1;
+                    bool result = int.TryParse(strbuilder.ToString(), out num);
+                    if (!result) error = true;
+                }
+                else if (constituents.Length == 2)
+                {
+                    bool result = int.TryParse(constituents[0].ToString(), out num);
+                    if (!result) error = true;
+                    result = int.TryParse(constituents[1].ToString(), out denom);
+                    if (!result || denom == 0) error = true;
+                }
+                else error = true;
+            } while (error);
+            this.numerator = num;
+            this.denominator = denom;
+            this.Simplify();
+        }
+    }
+}
---
+++ LR7/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace LR7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter fraction A: ");
+            Fraction A = new Fraction();
+            A.ConvertToFraction(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("Enter fraction B: ");
+            Fraction B = new Fraction();
+            B.ConvertToFraction(Console.ReadLine());
+            int choice;
+            do
+            {
+                Console.Clear();
+                Console.Write("Choose an action:\n1 - Find the sum of fractions\n2 - Find the difference of fractions\n" +
+                    "3 - Find the product of fractions\n4 - Find the quotient of fractions\n5 - Compare fractions\n" +
+                    "6 - Show information about fractions\n7 - Change fractions\n0 - Exit\n>> ");
+                choice = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine($"A + B = {A} + {B} = {A + B}");
+                        break;
+                    case 2:
+                        Console.WriteLine($"A - B = {A} - {B} = {A - B}");
+                        break;
+                    case 3:
+                        Console.WriteLine($"A * B = {A} * {B} = {A * B}");
+                        break;
+                    case 4:
+                        Console.WriteLine($"A / B = {A} / {B} = {A / B}");
+                        break;
+                    case 5:
+                        Console.WriteLine($"{A} & {B}");
+                        Console.WriteLine($"A == B : {A == B}");
+                        Console.WriteLine($"A != B : {A != B}");
+                        Console.WriteLine($"A > B : {A > B}");
+                        Console.WriteLine($"A < B : {A < B}");
+                        Console.WriteLine($"A >= B : {A >= B}");
+                        Console.WriteLine($"A <= B : {A <= B}");
+                        break;
+                    case 6:
+                        Console.WriteLine($"A = {A} = {A.ToSpecialString()} = {(double)A} ~ {(int)A}");
+                        Console.WriteLine($"B = {B} = {B.ToSpecialString()} = {(double)B} ~ {(int)B}");
+                        break;
+                    case 7:
+                        Console.WriteLine("Enter fraction A: ");
+                        A.ConvertToFraction(Console.ReadLine());
+                        Console.WriteLine("Enter fraction B: ");
+                        B.ConvertToFraction(Console.ReadLine());
+                        break;
+                    case 0: return;
+                    default:
+                        Console.WriteLine("Invalid input, try again");
+                        break;
+                }
+                Console.WriteLine("\nPress any button to continue");
+                Console.ReadKey();
+            } while (true);
+        }
+    }
+}
